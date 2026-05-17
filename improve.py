96dc7a1a95b93b2822f7ec5f6e86a2605165e39c
Harshit Malik <harshitfr25@gmail.com>
2026-05-17 16:57:04 +0000

    First few lines

---
+++ improve.py
@@ -0,0 +1,10 @@
+from src.setup import user_exists
+if user_exists = False:
+	print(Welcome to Improve!")
+	print("Improve is a CLI-tool designed to help people manage and organize their \n
+	ongoing projects and track their progress in them")
+	print("This project has been developed and open-sourced by harshit-malik25813 on GitHub"
+	print("For all those nerds, find this project on https://github.com/harshit-malik25813/Improve")
+	print("To start, setup your account by running 'improve setup' in your terminal")
+
+
